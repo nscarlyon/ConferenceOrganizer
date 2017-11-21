5bcce7196be6d493832b8126ddb7bdceb6226a84
nscarlyon <natasha.carlyon@gmail.com>
2017-11-20 20:12:32 -0500

    renname proposal.cs; add cfp class

--- ConferenceOrganizer.Data/Models.cs
+++ ConferenceOrganizer.Data/Models.cs
@@ -1,4 +1,7 @@
-﻿using MongoDB.Bson;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace ConferenceOrganizer.Data
@@ -12,5 +15,12 @@ namespace ConferenceOrganizer.Data
         public string title { get; set; }
         public string description { get; set; }
         public string email { get; set; }
+    }
+
+    public class CFP
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public string status { get; set; }
     }
 }

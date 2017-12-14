dad791c65c49cb450b81b972637c9e333d0da2e2
nscarlyon <natasha.carlyon@gmail.com>
2017-12-13 22:55:46 -0500

    move CFP and Proposal to seperate files

---
+++ ConferenceOrganizer.Data/CFP.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ConferenceOrganizer.Data
+{
+    public class CFP
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public string status { get; set; }
+    }
+}
--- ConferenceOrganizer.Data/Models.cs
+++ ConferenceOrganizer.Data/Models.cs
@@ -1,26 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace ConferenceOrganizer.Data
 {
-    public class Proposal
-    {
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string id { get; set; }
-        public string speakerName { get; set; }
-        public string bio { get; set; }
-        public string title { get; set; }
-        public string description { get; set; }
-        public string email { get; set; }
-    }
 
-    public class CFP
-    {
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string id { get; set; }
-        public string status { get; set; }
-    }
+
 }

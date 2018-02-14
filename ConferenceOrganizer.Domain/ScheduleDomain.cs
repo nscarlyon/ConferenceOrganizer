c8e3c2e7faa21a08a52666c3160e6a782ec25eb9
nscarlyon <natasha.carlyon@gmail.com>
2018-02-13 21:55:25 -0500

    schedule domain addRoom method

--- ConferenceOrganizer.Domain/ScheduleDomain.cs
+++ ConferenceOrganizer.Domain/ScheduleDomain.cs
@@ -33,5 +33,37 @@ namespace ConferenceOrganizer.Domain
             conferenceOrganizerDatabase.DeleteSchedule();
         }
 
+
+        public void AddRoom(string id, Rooms rooms)
+        {
+            var schedule = GetSchedule();
+            if (schedule.timeSlots.Count() == 0)
+            {
+            }
+            else if(schedule.rooms.Count() != rooms.rooms.Count())
+            {
+                var newRoom = rooms.rooms.Except(schedule.rooms).First();
+
+                foreach (var timeSlot in schedule.timeSlots)
+                {
+                    var splitTimeSlot = timeSlot.timeSlot.Split('-');
+                    int.TryParse(splitTimeSlot[0].Split(':')[0], out int startHour);
+                    int.TryParse(splitTimeSlot[0].Split(':')[1], out int startMin);
+                    int.TryParse(splitTimeSlot[1].Split(':')[0], out int endHour);
+                    int.TryParse(splitTimeSlot[1].Split(':')[1], out int endMin);
+                    var session = new Session
+                    {
+                        room = newRoom,
+                        startHour = startHour,
+                        startMin = startMin,
+                        endHour = endHour,
+                        endMin = endMin
+                    };
+                    Console.WriteLine(session.startHour);
+                    conferenceOrganizerDatabase.PostSession(session);
+                }
+            }
+            conferenceOrganizerDatabase.AddRoomsToSchedule(id, rooms);
+        }
     }
 }
